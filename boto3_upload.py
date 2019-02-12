94248c5172afdda289df86a73c4bb19b0d62f79b
Evan <evanpiercy@gmail.com>
2019-02-12 11:42:46 -0800

    added boto3 script. Minor UI updates

--- boto3_upload.py
+++ boto3_upload.py
@@ -1,15 +1,20 @@
 import boto3, io, zipfile, mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.codecorax.com')
+def lambda_handler(event, context):
 
-build_bucket = s3.Bucket('portfoliobuild.codecorax.com')
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('portfolio.codecorax.com')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    build_bucket = s3.Bucket('portfoliobuild.codecorax.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        return 'Unauthorized Response Detected. Engaging Nuclear Tactical Weapons.'
+    
