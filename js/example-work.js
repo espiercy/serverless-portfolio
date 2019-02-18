8222d4b4906b055888ce8a93c5a4d8ae0d5735ee
Evan <evanpiercy@gmail.com>
2019-02-18 09:34:33 -0800

    added testing

--- js/example-work.js
+++ js/example-work.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
 
 class ExampleWork extends React.Component {
   render() {
@@ -38,4 +45,6 @@ class ExampleWorkBubble extends React.Component{
     )
   }
 }
+
 export default ExampleWork;
+export { ExampleWorkBubble };
